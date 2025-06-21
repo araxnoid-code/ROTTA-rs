d4349210fa26ea81b52c00298fb8890f9b4389f2
arax <araxnoid0@gmail.com>
2025-06-22 00:21:05 +0700

    process to optimaze the math operation on tensor(now optimazing add function)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use std::time::SystemTime;
+use std::time::{ SystemTime, UNIX_EPOCH };
 
-use crate::rotta_rs::{ sum_arr, Arrayy };
+use crate::rotta_rs::{ sum_arr, Arrayy, Tensor };
 
 mod rotta_rs;
 
 fn main() {
-    let arr = Arrayy::from_vector(vec![2, 3], vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
+    let tensor = Tensor::new([
+        [1.0, 2.0, 3.0],
+        [4.0, 5.0, 6.0],
+    ]);
 
-    println!("{}", arr.sum())
+    let skalar = Tensor::new([[[1.0]]]);
+
+    (&tensor + &skalar).backward();
+
+    println!("{}", skalar.grad())
 }
--- src/rotta_rs/tensor/operation/add.rs
+++ src/rotta_rs/tensor/operation/add.rs
@@ -5,33 +5,71 @@ use crate::rotta_rs::{
     broadcasting_tensor_non_panic,
     Arrayy,
     BackwardLabel,
+    MultipleSum,
     NodeType,
     Tensor,
 };
 
 // add
 pub fn add(a: &Tensor, b: &Tensor) -> Tensor {
-    let broadcast_shape = broadcast_concat(&a.value(), &b.value());
+    let a_arr = a.value();
+    let b_arr = b.value();
 
-    let broadcast_a = broadcasting_tensor_non_panic(a, broadcast_shape.clone());
-    let broadcast_b = broadcasting_tensor_non_panic(b, broadcast_shape);
+    // skalar
+    if a_arr.shape.multiple_sum() == 1 || b_arr.shape.multiple_sum() == 1 {
+        let output = a_arr + b_arr;
 
-    let output = broadcast_a.value() + broadcast_b.value();
-    let tensor = Tensor::from_arrayy(output);
-    tensor.update_parent(vec![broadcast_a.node.clone(), broadcast_b.node.clone()]);
-    tensor.node.lock().as_mut().unwrap().label = Some(
-        BackwardLabel::Add(broadcast_a.node.clone(), broadcast_b.node.clone())
-    );
+        let tensor = Tensor::from_arrayy(output);
+        tensor.update_parent(vec![a.node.clone(), b.node.clone()]);
+        tensor.node.lock().as_mut().unwrap().label = Some(
+            BackwardLabel::Add(a.node.clone(), b.node.clone())
+        );
 
-    tensor
+        tensor
+    } else if a_arr.shape == b_arr.shape {
+        let output = a_arr + b_arr;
+
+        let tensor = Tensor::from_arrayy(output);
+        tensor.update_parent(vec![a.node.clone(), b.node.clone()]);
+        tensor.node.lock().as_mut().unwrap().label = Some(
+            BackwardLabel::Add(a.node.clone(), b.node.clone())
+        );
+
+        tensor
+    } else {
+        let broadcast_shape = broadcast_concat(&a.value(), &b.value());
+
+        let broadcast_a = broadcasting_tensor_non_panic(a, broadcast_shape.clone());
+        let broadcast_b = broadcasting_tensor_non_panic(b, broadcast_shape);
+
+        let output = broadcast_a.value() + broadcast_b.value();
+        let tensor = Tensor::from_arrayy(output);
+        tensor.update_parent(vec![broadcast_a.node.clone(), broadcast_b.node.clone()]);
+        tensor.node.lock().as_mut().unwrap().label = Some(
+            BackwardLabel::Add(broadcast_a.node.clone(), broadcast_b.node.clone())
+        );
+
+        tensor
+    }
 }
 
 pub fn d_add(a: &NodeType, b: &NodeType, grad: Arrayy) {
     // f/da = 1 * grad = grad
-    a.lock().as_mut().unwrap().add_grad(grad.clone());
+
+    let d_a = if a.lock().unwrap().value.shape.multiple_sum() == 1 {
+        Arrayy::from_vector(a.lock().unwrap().value.shape.clone(), vec![grad.sum()])
+    } else {
+        grad.clone()
+    };
+    a.lock().as_mut().unwrap().add_grad(d_a);
 
     // f/db = 1 * grad = grad
-    b.lock().as_mut().unwrap().add_grad(grad);
+    let d_b = if b.lock().unwrap().value.shape.multiple_sum() == 1 {
+        Arrayy::from_vector(b.lock().unwrap().value.shape.clone(), vec![grad.sum()])
+    } else {
+        grad.clone()
+    };
+    b.lock().as_mut().unwrap().add_grad(d_b);
 }
 
 // method
