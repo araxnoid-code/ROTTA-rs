89e8dd23da2a8f1f4ba9e2bf158980d79d28c9de
arax <araxnoid0@gmail.com>
2025-07-16 19:51:03 +0700

    +TensorRange and implemented to Tenso::arange

--- src/main.rs
+++ src/main.rs
@@ -35,51 +35,9 @@ use std::time::SystemTime;
 use rotta_rs::{ arrayy::r, * };
 
 fn main() {
-    let input = Tensor::arange(0, 250, 1).reshape(vec![250, 1]);
-    let label = (&Tensor::arange(0, 250, 1) * 10.0).reshape(vec![250, 1]);
-
-    let mut model = Module::init();
-    model.update_initialization(rotta_rs::WeightInitialization::He);
-
-    let linear_a = model.liniar_init(1, 512);
-    // let mut droput = model.dropout_init(0.3);
-    let linear_b = model.liniar_init(512, 512);
-
-    let linear_c = model.liniar_init(512, 1);
-
-    let loss_fn = MAE::init();
-    let mut optimazer = Adam::init(model.parameters(), 0.00001);
-
-    let tick = std::time::SystemTime
-        ::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap()
-        .as_millis();
-
-    for epoch in 0..500 {
-        let x = linear_a.forward(&input);
-        let x = relu(&x);
-
-        let x = linear_b.forward(&x);
-        let x = relu(&x);
-        // let x = droput.forward(&x);
-
-        let x = linear_c.forward(&x);
-
-        let loss = loss_fn.forward(&x, &label);
-        println!("epoch:{epoch} | loss => {}", loss);
-
-        optimazer.zero_grad();
-
-        let backward = loss.backward();
-
-        optimazer.optim(backward);
-    }
-
-    let tock = std::time::SystemTime
-        ::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap()
-        .as_millis();
-    println!("{}ms", tock - tick)
+    let tensor = Tensor::arange(0..10)
+        .step(2)
+        .map(|x| { x * 2.0 })
+        .collect();
+    println!("{}", tensor);
 }
