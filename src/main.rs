c1c3c4bd1218803f47dae1cf10caa686be05b09e
arax <araxnoid0@gmail.com>
2025-07-17 22:30:37 +0700

    +change add mechanism for same shape

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
+use std::time::SystemTime;
+
 use rotta_rs::Tensor;
 
 fn main() {
-    let tensor = Tensor::arange(0..20)
-        .step(2)
-        .map(|x| { x * 2.0 })
-        .to_shape(vec![2, 5])
-        .collect();
-    println!("{}", tensor);
+    let length = 1_000_000_00;
+    let tensor_a = Tensor::from_vector(vec![length], vec![0.0;length]);
+    let tensor_b = Tensor::from_vector(vec![length], vec![100.0;length]);
+
+    let tick = std::time::SystemTime
+        ::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_millis();
+
+    let _ = &tensor_a + &tensor_b;
+
+    let tock = std::time::SystemTime
+        ::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_millis();
+
+    println!("{}ms", tock - tick);
 }
