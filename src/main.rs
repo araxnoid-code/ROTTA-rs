95f1f7528fd70a83bfe4843b19b84cbdfddf9a9f
arax <araxnoid0@gmail.com>
2025-07-24 22:11:35 +0700

    testing reshape peform

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,4 @@ use std::time::SystemTime;
 
 use rotta_rs::Tensor;
 
-fn main() {
-    let tensor = Tensor::rand(vec![256, 256, 1024]);
-
-    println!("{}", "bachmark running");
-    let tick = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis();
-    tensor.flatten();
-    let tock = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis();
-
-    println!("{}ms", tock - tick);
-}
+fn main() {}
